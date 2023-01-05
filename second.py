22620c77734ae6525ccf2902bdd8a7e87c77f8bc
bhvikjain846 <jainbhvik846@gmail.com>
2023-01-05 15:44:01 +0530

    Workflow with 2 jobs

--- second.py
+++ second.py
@@ -1,8 +1,1 @@
-#binary to decimal
-b=input("Enter a binary no.: ")
-def btod(b):
-    d = 0
-    for number in b:
-        d = d*2 + int(number)
-    print(d)
-btod(b)
+print("My name is Bhvik Jain")
