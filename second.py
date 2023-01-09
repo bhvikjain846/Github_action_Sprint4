748e399ed71e94700b84ecec98de67569c0e0fc4
bhvikjain846 <jainbhvik846@gmail.com>
2023-01-09 14:07:32 +0530

    Input through workflow

--- second.py
+++ second.py
@@ -1,9 +1,14 @@
-# binary to decimal
 import sys
-b=int(sys.argv[2])
-def btod(b):
-    d = 0
-    for number in b:
-        d = d*2 + int(number)
-    print(d)
-btod(b)
+num = int(sys.argv[2])
+flag = False
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
